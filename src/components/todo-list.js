fb14296dd512fac8230f1d55564dd84a35294d54
zhora <vikulov.georgiy@wildberries.ru>
2019-07-26 18:04:24 +0300

    Ключи и коллекции

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -3,21 +3,30 @@
 import React from "react";
 import TodoListItem from "./todo-list-item";
 const TodoList = ({ todos }) => {
-    const items = ['Learn React', 'Build Awesome App']
 /*
     Массив можно передать как свойство
     В JSX можно вставлять массивы элементов (не только по одному)
     Можно передать все свойства объекта в компонент используя Object spread оператор не перечисляя каждое
 */
 
+    // Вместо того что бы сравнивать элементы один за другим React смотрит на уникальные ключи сопоставляя элементы между
+    //собой тем самым если ключа нет реакт добавит <li> точечно не обновляя весь ul если ключ есть то реакт не будет обновлять элемент
+    //Вывод нет ни какого смысла использовать индекс элемента в массиве потому что по умолчанию  не получим производительность за которую борится реакт
+
+    //Вывод каждому JSX элементу в массиве нужно уникальное свойство key
+    // React использует key что бы эффективно сравнивать элементы при обновлении
+    // Не стоит делать ключи из индекса массива
     const elements = todos.map((item) => {
+        // В свойстве  itemProps будут все свойства кроме id
+        const { id, ...itemProps } = item
        return (
-           <li>
+           <li key={ id }>
                <TodoListItem
+                   /* Не будем передавать id в spread оператор */
                    // label={item.label}
                    // important={item.important}
                    /*Используем spread оператор*/
-                   { ...item }
+                   { ...itemProps }
                />
            </li>
        )
@@ -28,10 +37,6 @@ const TodoList = ({ todos }) => {
             { elements }
         </ul>
     )
-}
-
-const AppHeader = () => {
-    return <h1>My Todo List</h1>
 }
 
 export default TodoList
