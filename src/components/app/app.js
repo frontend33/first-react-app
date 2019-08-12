b1241f8ab45511dc5a0114323e8eb480ff5f8337
zhora <vikulov.georgiy@wildberries.ru>
2019-08-12 17:59:04 +0300

    delete element from setState

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,34 +1,56 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import AppHeader from '../app-header'
 import TodoList from '../todo-list'
 import SearchPanel from '../search-panel/search-panel'
 import ItemStatusFilter from '../item-status-filter';
 import './app.css'
-const App = () => {
-  const todoData = [
-    { label: 'Drink Coffee', important: false, id: 1 },
-    { label: 'Make Awesome App', important: true, id: 2 },
-    { label: 'Have a lunch', important: false, id: 3 },
-  ]
 
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={3} />
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
+export default class App extends Component {
+  state = {
+    todoData: [
+      { label: 'Drink Coffee', important: false, id: 1 },
+      { label: 'Make Awesome App', important: true, id: 2 },
+      { label: 'Have a lunch', important: false, id: 3 },
+    ]
+  }
+  deleteItem = (id) => {
+    console.log('del', id)
+    this.setState(({ todoData }) => {
+      const idx = todoData.findIndex((el) => el.id === id)
+      // setState не должен изменять текущий state  todoData.splice(idx, 1) - Не изменяем массив напрямую правила реакта
+      // Методы которые изменяют (mutate) массив использовать нельзя
+      // const newArray = [...before, ...after]
+      const newArray = [
+        ...todoData.slice(0, idx),
+        ...todoData.slice(idx + 1)
+      ];
+      return {
+        todoData: newArray
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div className="todo-app">
+        <AppHeader toDo={1} done={3} />
+        <div className="top-panel d-flex">
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
+
+        <TodoList todos={this.state.todoData}
+          /* Любой компонент может генерировать собственные события (onDone, onAdded)
+          Достаточно передать callback функцию как property, а затем вызвать ее из компонента когда наступило событие
+          Через события данные поднимаются вверх по иерархии компонентов
+          */
+          onDeleted={this.deleteItem}
+        />
       </div>
+    );
+  }
 
-      <TodoList todos={todoData}
-      /* Любой компонент может генерировать собственные события (onDone, onAdded)
-      Достаточно передать callback функцию как property, а затем вызвать ее из компонента когда наступило событие
-      Через события данные поднимаются вверх по иерархии компонентов
-      */
-      onDeleted={ (id) => console.log('del', id) }
-      />
-    </div>
-  );
 };
 
 /* jsx Позволяет использовать выражения {foo.bar}
@@ -37,4 +59,4 @@ class = className, for = htmlFor
 В свойства можно передавать любое значение
 null, undefined, true и false в теле тегов игнорируются не вызывая ошибок
 */
-export default App
+// export default App
